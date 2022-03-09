1c05fcc92085434e646f43ffb134605e830775dd
Alesja <graviella@mail.ru>
2022-03-09 08:55:40 +0300

    add: add mobile menu

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ let buttonCall = document.querySelector('.nav__btn-to-form'),
     closeForm = document.querySelector('.order-form__close'),
     keyUp = -1
 
+// open modal order
 buttonCall.addEventListener('click', () => {
   modalForm.classList.toggle('modal-order-phone_visible')
   form.classList.add('move-down')
-  let keyDown = setTimeout(function() {
+  setTimeout(function() {
     form.style.top = '0'
   }, 300)
   body.classList.add('hidden')
@@ -30,7 +31,74 @@ function closeModalForm() {
     form.style.top = '-100%'
     form.classList.toggle('move-down')
     form.classList.toggle('move-up')
+    body.classList.remove('hidden')
   }, 1000)
 }
 
 closeForm.addEventListener('click', closeModalForm)
+
+//open mobile menu
+let burger = document.querySelector('.nav__burger'),
+    mobMenu = document.querySelector('.nav__link-social')
+
+burger.addEventListener('click', () => {
+  body.classList.add('hidden-mob')
+  setTimeout(function() {
+    mobMenu.classList.add('nav__link-social_mob-down')
+    setTimeout(() => {
+      mobMenu.classList.add('bottom')
+      mobMenu.classList.remove('nav__link-social_mob-down')
+    }, 1000);
+  }, 300)
+})
+
+// close mob menu
+let mobBody = document.querySelector('.nav__link-social'),
+    startY = 0,
+    distanceY = 0,
+    moveY = false
+
+// mobile swipe-menu
+function closeMobMenu() {
+  mobBody.classList.add('nav__link-social_mob-up')
+    setTimeout(() => {
+      mobMenu.classList.remove('bottom')
+      mobMenu.classList.remove('nav__link-social_mob-up')
+      body.classList.remove('hidden-mob')
+    }, 1000);
+}
+
+mobBody.addEventListener('touchstart', function(e) {
+  startY = e.touches[0].clientY;
+})
+
+mobBody.addEventListener('touchmove', function(e) {
+  let coordY = e.touches[0].clientY
+  distanceY = coordY- startY
+  moveY = true
+
+})
+
+mobBody.addEventListener('touchend', function(e) {
+  if(moveY && distanceY < 0) {
+    closeMobMenu()
+    startY = 0
+    moveY = true
+  }
+})
+
+//mob menu close by clicking on overlay
+//mobOverlay = document.querySelector('.hidden-mob')
+body.addEventListener('click', (e) => {
+  let thisTarget = e.target
+
+
+  if(thisTarget.classList.contains('nav')) {
+    closeMobMenu()
+  }
+
+  let userWidth = document.documentElement.clientWidth
+  if(userWidth <= 1290 ) {
+    alert(thisTarget.tagName)
+  }
+})
